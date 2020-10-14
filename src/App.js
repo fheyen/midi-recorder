7651b46ae9d7efc2b53059324f21184739aac0f0
fheyen <frank.heyen@outlook.de>
2020-10-14 20:04:55 +0200

    added github link

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
 import './style/App.css';
 import Toolbar from './components/Toolbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default class App extends Component {
 
-    constructor(props) {
-        super(props);
-
-
-        this.state = {
-
-        };
-    }
-
     render() {
-
         return (
             <div className='App bright' >
                 <Toolbar />
+                <div className='githubLink'>
+                    <a href='https://github.com/fheyen/midi-recorder'>
+                        <FontAwesomeIcon icon={faGithub} />&nbsp;
+                        https://github.com/fheyen/midi-recorder
+                    </a>
+                </div>
             </div >
         );
     }
