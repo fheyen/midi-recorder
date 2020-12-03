4c0e9c7425a54bbe70b62c5d77071a8fdabda91a
fheyen <frank.heyen@outlook.de>
2020-12-03 11:43:47 +0100

    updated to newest version of create-react-app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react';
 import './style/App.css';
 import Toolbar from './components/Toolbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
-export default class App extends Component {
-
-    render() {
-        return (
-            <div className='App bright' >
-                <Toolbar />
-                <div className='githubLink'>
-                    <a href='https://github.com/fheyen/midi-recorder'>
-                        <FontAwesomeIcon icon={faGithub} />&nbsp;
+function App() {
+  return (
+    <div className='App bright' >
+      <Toolbar />
+      <div className='githubLink'>
+        <a href='https://github.com/fheyen/midi-recorder'>
+          <FontAwesomeIcon icon={faGithub} />&nbsp;
                         https://github.com/fheyen/midi-recorder
-                    </a>
-                </div>
-            </div >
-        );
-    }
+          </a>
+      </div>
+    </div >
+  );
 }
+
+export default App;
